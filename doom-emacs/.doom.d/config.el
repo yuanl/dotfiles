ad120293920ba64d9a62cc4993168e321a69499b
Leonard <leonard.li@sony.com>
2018-09-10 14:33:37 +0800

    switch to new font Iosevka.

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -1,6 +1,10 @@
 ;;; ~/dotfiles/doom-emacs/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(put 'dired-find-alternate-file 'disabled nil)
+(setq doom-font (font-spec :family "Iosevka" :size 16))
+
+(add-hook! dired-mode
+  (put 'dired-find-alternate-file 'disabled nil))
+
 ;; (org-babel-do-load-languages
 ;;  'org-babel-load-languages
 ;;  '((python     . t)

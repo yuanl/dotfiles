04b73e6c1575b907e9b34157203761f7a2c5b090
Leonard <leonard.li@sony.com>
2018-11-27 16:14:35 +0800

    refine emacs customize.

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -2,8 +2,10 @@
 
 (setq doom-font (font-spec :family "Iosevka" :size 16))
 
-(add-hook! dired-mode
-  (put 'dired-find-alternate-file 'disabled nil))
+(def-package! dired
+  :config
+  (put 'dired-find-alternate-file 'disabled nil)
+  )
 
 ;; (org-babel-do-load-languages
 ;;  'org-babel-load-languages

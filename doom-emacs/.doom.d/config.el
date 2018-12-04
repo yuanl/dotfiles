e884f04613fc0c830313a7941e4636236fe4d69e
Leonard <leonard.li@sony.com>
2018-12-04 15:39:10 +0800

    try exwm.

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -7,11 +7,27 @@
   (put 'dired-find-alternate-file 'disabled nil)
   )
 
+(def-package! eshell
+  :bind (("C-c C-e". eshell))
+  )
+
 (def-package! elpy
   :config
   (setq python-shell-interpreter "ipython"
         python-shell-interpreter-args "-i --simple-prompt")
   )
+
+(def-package! org-jira
+  :config
+  (setq jiralib-url "http://jira.sonymobile.net/")
+  )
+
+;; (use-package exwm
+;;   ;:disabled f
+;;   :init
+;;   (require 'exwm-config)
+;;   (exwm-config-default)
+;;   )
 
 ;; (org-babel-do-load-languages
 ;;  'org-babel-load-languages
--- doom-emacs/.doom.d/packages.el
+++ doom-emacs/.doom.d/packages.el
@@ -4,9 +4,11 @@
 ;; * Disable
 ;; (package! org-bullets :disable t)
 ;; * Lang
+;;(package! exwm)
 (package! docker)
 (package! dockerfile-mode)
 (package! docker-compose-mode)
+(package! elpy)
 (package! graphviz-dot-mode)
 (package! tide)
 (package! vue-html-mode)

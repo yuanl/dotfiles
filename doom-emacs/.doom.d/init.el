4f58f170ef37cea0fc4d976bba4b0b2bdb5a023e
YuanL.Lee <lyuanl@gmail.com>
2019-01-21 02:45:13 +0800

    get rid of evil binding.

--- doom-emacs/.doom.d/init.el
+++ doom-emacs/.doom.d/init.el
@@ -4,7 +4,6 @@
 (doom! :feature
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
-       (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
@@ -159,4 +158,4 @@
        ;; The default module sets reasonable defaults for Emacs. It also
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
-       (default +bindings +smartparens +evil-bindings))
+       (default +bindings +smartparens +emacs-bindings))

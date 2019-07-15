c6daaa53b030d58390d93fbce99e7117ce6e045d
YuanL.Lee <lyuanl@gmail.com>
2019-07-15 10:52:56 +0800

    fish: add source env file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,4 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths

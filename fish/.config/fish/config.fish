29334fe548702b955de2d4b0b4abdc2de69420aa
Leonard <leonard.li@sony.com>
2020-01-02 13:18:51 +0800

    fish: disable greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,10 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# Disable greeting
+set fish_greeting
+
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 function fish_title

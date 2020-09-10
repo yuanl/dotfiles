95a1d175ae543cf90195e1534e606f67f387696c
Yuanliang Li <yuanl.lee@gmail.com>
2020-09-10 11:04:52 +0800

    pyenv: test if exist virtualenv

--- fish/.config/fish/conf.d/pyenv.fish
+++ fish/.config/fish/conf.d/pyenv.fish
@@ -2,5 +2,9 @@ if test -d $HOME/.pyenv
    set -g pyenv_root $HOME/.pyenv
    set -g fish_user_paths $pyenv_root/bin $fish_user_paths
    status --is-interactive; and pyenv init - | source
-   status --is-interactive; and pyenv virtualenv-init - | source
+
+   if test -d $HOME/.pyenv/plugins/pyenv-virtualenv
+      status --is-interactive; and pyenv virtualenv-init - | source
+   end
+
 end

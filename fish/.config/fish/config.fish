9ecc1135bf780e4e1cb35112a8f14b0d0c2203a5
Yuanliang Li <yuanl.lee@gmail.com>
2020-10-29 13:48:46 +0800

    cleanup fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,5 @@
 # fish is the best
 
-set fish_greeting
-
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish

4322d8b79debb8ff92b0d967fabc9f0b0eb2ef13
Yuanliang Li <yuanl.lee@gmail.com>
2021-05-26 10:28:15 +0800

    fish: add script var to set proxy ip

--- fish/.config/fish/conf.d/proxy.fish
+++ fish/.config/fish/conf.d/proxy.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/env fish
 
-alias goproxy="export https_proxy=http://192.168.1.1:7890 http_proxy=http://192.168.1.1:7890 all_proxy=socks5://192.168.1.1:7891"
+# IP address of socks5 proxy server.
+# set ADDRESS "192.168.1.1"
+set ADDRESS "127.0.0.1"
+
+alias goproxy="export https_proxy=http://$ADDRESS:7890 http_proxy=http://$ADDRESS:7890 all_proxy=socks5://$ADDRESS:7891"
 alias disproxy="set -e http_proxy; set -e https_proxy; set -e all_proxy"
 
 goproxy

3d8137e8f3ccd28e01acd93cb53a7515c842b5d2
Yuanliang Li <yuanl.lee@gmail.com>
2021-08-06 10:12:11 +0800

    fish: enable aws cli autocompletion

---
+++ fish/.config/fish/conf.d/aws.fish
@@ -0,0 +1,4 @@
+if test -x /usr/local/bin/aws
+    # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+    complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+end

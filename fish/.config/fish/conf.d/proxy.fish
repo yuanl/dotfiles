329d67cb1608d95ae4ff210cf9630c3e0246ec56
Yuanliang Li <yuanl.lee@gmail.com>
2021-08-06 10:11:35 +0800

    add proxy setting in fish

--- fish/.config/fish/conf.d/proxy.fish
+++ fish/.config/fish/conf.d/proxy.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 
 # IP address of socks5 proxy server.
-# set ADDRESS "192.168.1.1"
-set ADDRESS "127.0.0.1"
+set ADDRESS "192.168.1.1"
+# set ADDRESS "127.0.0.1"
 
 alias goproxy="export https_proxy=http://$ADDRESS:7890 http_proxy=http://$ADDRESS:7890 all_proxy=socks5://$ADDRESS:7891"
 alias disproxy="set -e http_proxy; set -e https_proxy; set -e all_proxy"

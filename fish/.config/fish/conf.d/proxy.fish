fba01832843fb2c617f0b334d165bee0e678bc07
Li <lyuanli@88665a3c66aa.ant.amazon.com>
2022-01-06 11:42:33 +0000

    clean up

--- fish/.config/fish/conf.d/proxy.fish
+++ fish/.config/fish/conf.d/proxy.fish
@@ -7,4 +7,4 @@ set ADDRESS "192.168.1.1"
 alias goproxy="export https_proxy=http://$ADDRESS:7890 http_proxy=http://$ADDRESS:7890 all_proxy=socks5://$ADDRESS:7891"
 alias disproxy="set -e http_proxy; set -e https_proxy; set -e all_proxy"
 
-goproxy
+#goproxy

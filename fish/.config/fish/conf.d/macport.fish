eaa6866f1c298d6f2d5e6b3268a2e185a4be7925
Yuanliang <yuanl.lee@gmail.com>
2023-09-04 19:44:42 +0100

    switch between emacs{,-mac}

--- fish/.config/fish/conf.d/macport.fish
+++ fish/.config/fish/conf.d/macport.fish
@@ -1,9 +1,11 @@
+set EMACS /Applications/MacPorts/EmacsMac.app
+
 if test -d /opt/local/bin
     set -g fish_user_paths /opt/local/bin /opt/local/libexec/gnubin $fish_user_paths
-    set -gx EDITOR "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin/emacsclient"
+    set -gx EDITOR "$EMACS/Contents/MacOS/bin/emacsclient"
 end
 
-if test -d /Applications/MacPorts/Emacs.app
-    set -g fish_user_paths /Applications/MacPorts/Emacs.app/Contents/MacOS/ $fish_user_paths
+if test -d {$EMACS}
+    set -g fish_user_paths {$EMACS}/Contents/MacOS/ $fish_user_paths
 end
 
